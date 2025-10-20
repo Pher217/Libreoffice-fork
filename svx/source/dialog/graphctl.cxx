takahiro-itou/core

svx: change SdrPage size and border to use gfx::Length
Change-Id: I382cfba6189eab02581057ab5af437cd1d163138

svx/source/dialog/graphctl.cxx
@@ -127,7 +127,7 @@ void GraphCtrl::InitSdrModel()
 
     pPage = new SdrPage( *pModel );
 
-    pPage->SetSize( aGraphSize );
+    pPage->setToolsSize(aGraphSize);
     pPage->SetBorder( 0, 0, 0, 0 );
     pModel->InsertPage( pPage.get() );
     pModel->SetChanged( false );

Pher217/Libreoffice-fork

svx/source/dialog/graphctl.cxx
@@ -126,7 +126,7 @@ void GraphCtrl::InitSdrModel()
 
     pPage = new SdrPage( *mpModel );
 
-    pPage->SetSize( maGraphSize );
+    pPage->setToolsSize(maGraphSize);
     pPage->SetBorder( 0, 0, 0, 0 );
     mpModel->InsertPage( pPage.get() );
     mpModel->SetChanged( false );